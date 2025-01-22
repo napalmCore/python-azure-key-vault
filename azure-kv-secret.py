aca70dde2a5cc951de85cbf2bd928a0fac59dc62
Aymen Mejri <mejria@gmail.com>
2025-01-22 14:43:35 -0500

    Stored key vault params into env file

--- azure-kv-secret.py
+++ azure-kv-secret.py
@@ -1,13 +1,15 @@
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from azure.identity import ClientSecretCredential
-
+from dotenv import load_dotenv
+import os
+load_dotenv('.env')
 
 # Replace with your Key Vault URL
-key_vault_url = "<your-azure-key-vault-domaine>"
-client_id = "<client-id>"
-tenant_id = "<tenant-id>"
-client_secret = "<client-secret>"
+key_vault_url = os.getenv('KEY_VAULT_DOMAIN')
+client_id = os.getenv('CLIENT_ID')
+tenant_id = os.getenv('TENANT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
 
 # Authenticate using DefaultAzureCredential
 credential = ClientSecretCredential(client_id=client_id, tenant_id=tenant_id, client_secret=client_secret)
@@ -16,7 +18,7 @@ credential = ClientSecretCredential(client_id=client_id, tenant_id=tenant_id, cl
 client = SecretClient(vault_url=key_vault_url, credential=credential)
 
 # Replace with the name of the secret you want to retrieve
-secret_name = "test-secret"
+secret_name = os.getenv('SECRET_NAME')
 
 try:
     # Retrieve the secret
